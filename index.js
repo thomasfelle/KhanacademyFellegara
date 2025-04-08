75590cdfb2c8ee1eb2b00f80b882f515d86aecf7
thomasfelle <stud.fellegara@gdromagnosi.edu.it>
2025-04-08 14:27:32 +0000

    terza_pagina

---
+++ index.js
@@ -0,0 +1,22 @@
+mouseClicked = function() {
+    // Scene 2
+    background(173, 239, 255);
+    fill(7, 14, 145);
+    textSize(25);
+    text("Una creatura è nata", 10, 100);
+    image(getImage("creatures/BabyWinston"), 80, 150);
+};
+
+// Scene 1
+background(235, 247, 255);
+fill(0, 85, 255);
+textSize(25);
+text("La storia della nascita", 10, 200);
+mouseClicked = function() {
+    // Scene 2
+    background(173, 239, 255);
+    fill(7, 14, 145);
+    textSize(25);
+    text("Una creatura è nata", 10, 100);
+    image(getImage("creatures/BabyWinston"), 80, 150);
+};
